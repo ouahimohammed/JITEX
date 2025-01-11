b71825cd78d53851186ad14438e784d57dfcec13
ouahimohammed <ouahi49@gmail.com>
2025-01-11 14:34:38 +0100

    nouveau api

--- src/actions/volsActions.js
+++ src/actions/volsActions.js
@@ -2,7 +2,7 @@ export const fetchVols = () => async (dispatch) => {
   dispatch({ type: 'FETCH_VOLS_START' });
   
   try {
-    const response = await fetch('https://volapi.vercel.app/api/vols'); // Utilise '/api/vols' car Vite redirigera cette requête vers l'API
+    const response = await fetch('https://volsapii.vercel.app/api/vols'); // Utilise '/api/vols' car Vite redirigera cette requête vers l'API
     if (!response.ok) {
       throw new Error('Failed to fetch flights');
     }
