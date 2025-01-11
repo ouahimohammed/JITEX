276772d55d87d70f6ba85996af977db093816c8e
ouahimohammed <ouahi49@gmail.com>
2025-01-11 16:58:03 +0100

    gg

--- src/components/VolsReservation.jsx
+++ src/components/VolsReservation.jsx
@@ -171,7 +171,7 @@ const VolsReservation = () => {
   const handleConfirm = async () => {
     setIsLoading(true);
     try {
-      const response = await fetch("https://volsapii.vercel.app/api/checkout", {
+      const response = await fetch("http://localhost:3000/api/checkout", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
